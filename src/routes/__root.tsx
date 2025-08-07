d4b8a119fd423d2ededae3888968c4097005dadb
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2025-08-07 01:59:51 +0100

    Create an Ideas Page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,21 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 
 export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        name: "description",
+        contest: "Test project using React + TanStack Router"
+      },
+      {
+        title: "IdeaDrop - Your Idea Hub"
+      }
+    ]
+  }),
   component: () => (
     <>
+      <HeadContent />
       <Outlet />
       <TanStackRouterDevtools />
     </>
