50ef3ab35f3a5b25277eb170db9bab3e06d1d630
Mohamad Yabroudi <mohamad.syr7@gmail.com>
2025-08-07 15:09:26 +0100

    Add a NotFound component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { HeadContent, Outlet, createRootRouteWithContext } from '@tanstack/react-router'
+import { HeadContent, Link, Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 import { QueryClient } from '@tanstack/react-query'
 import Header from '@/components/Header';
@@ -20,6 +20,7 @@ export const Route = createRootRouteWithContext<RouterContext>()({
     ]
   }),
   component: RootLayout,
+  notFoundComponent: NotFound,
 })
 
 function RootLayout() {
@@ -35,6 +36,25 @@ function RootLayout() {
           </div>
         </main>
       <TanStackRouterDevtools />
+    </div>
+  );
+}
+
+function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center text-center py-20">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">
+        404
+      </h1>
+      <p className="text-lg text-gray-600 mb-6">
+        Oops! The page you are looking for does not exist
+      </p>
+      <Link
+        to="/"
+        className='bg-blue-600 text-white rounded px-4 py-2 hover:bg-blue-700 transition'
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 }
